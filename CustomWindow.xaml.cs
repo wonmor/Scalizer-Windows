6455f0365bfd3408dc23998e7c88c87d632e5f07
John Seong <wonmor@gmail.com>
2022-11-13 14:49:24 -0500

    Update CustomWindow.xaml.cs

--- CustomWindow.xaml.cs
+++ CustomWindow.xaml.cs
@@ -22,6 +22,7 @@ using System.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Scalizer
 {
@@ -129,6 +130,39 @@ namespace Scalizer
 
                     Change_Window(sender, e);
                     break;
+
+                case "donateButton":
+                    OpenUrl("https://www.buymeacoffee.com/wonmor");
+                    break;
+            }
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
             }
         }
 
